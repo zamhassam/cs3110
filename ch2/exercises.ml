20237fda8f716edfe937a9172f6655eff54b960e
zameer.hassam <zameer.hassam@transficc.com>
2021-07-29 09:10:54 +0100

    completed the set of exercises for this chapter

--- ch2/exercises.ml
+++ ch2/exercises.ml
@@ -31,3 +31,32 @@ assert ((let rms x y = sqrt(((x*.x)+.(y*.y)/.2.)) in rms 2. 10.) > 7.3);;
 
 let valid_date d m =
   if ((m = "Jan" || m = "Mar" || m = "May" || m = "Jul" || m = "Aug" || m = "Oct" || m = "Dec") && d >= 1 && d <= 31) then true else if ((m = "Apr" || m = "June" || m = "Sep" || m = "Nov") && d >=1 && d <= 30) then true else if (m = "Feb" && d >= 1 && d <= 28) then true else false in valid_date 31 "Nov";;
+
+let rec fib n =
+  if n = 0 then 0
+  else if n = 1 then 1
+  else fib(n - 1) + fib(n - 2) in fib 4;;
+
+let fast_fib n =
+  let rec fib_helper i minus2 minus1 = if i = 0 then minus2 + minus1 else fib_helper (i - 1) minus1 (minus1 + minus2) in
+    if n = 0 then 0
+    else if n = 1 then 1
+    else fib_helper (n - 2) 0 1 in  fast_fib 4;;
+
+let rec divide (n:float) (d:float) =
+  if d > n then 0 else (1 + divide(n -. d) d) in divide 10. 3.;;
+
+let (+/.) (x:float) (y:float) =
+  (x +. y) /. 2. in 1. +/. 2.;;
+
+let rec print_int_list = function 
+| [] -> () 
+| h::t -> print_int h;
+          print_newline ();
+          print_int_list t in print_int_list [1;2;3];;
+
+let print_int_list' lst = 
+  List.iter (fun x -> 
+      print_int x;
+      print_newline ();
+    ) lst in print_int_list' [1;2;3];;
