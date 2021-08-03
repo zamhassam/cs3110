07b0cf250d4e32a9ec9fee00fa7b99063e7eabf8
zameer.hassam <zameer.hassam@transficc.com>
2021-08-03 11:09:01 +0100

    silly vscode isn't autosaving...

--- ch3/sum_test.ml
+++ ch3/sum_test.ml
@@ -5,7 +5,7 @@ let make_sum_test name expected_output input =
   name >:: (fun _ -> assert_equal expected_output (sum input) ~printer:string_of_int);
 
 let tests = "test suite for sum" >::: [
-  "empty" >:: (fun _ -> assert_equal 0 (sum []) ~printer:string_of_int);
+  make_sum_test "empty" >:: (fun _ -> assert_equal 0 (sum []) ~printer:string_of_int);
   "one" >:: (fun _ -> assert_equal 1 (sum [1]) ~printer:string_of_int);
   "onetwo" >:: (fun _ -> assert_equal 3 (sum [1; 2]) ~printer:string_of_int);
 ]
