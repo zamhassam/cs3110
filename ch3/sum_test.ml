16619372dd80c524887f20f2022289c7cc4bcafb
zameer.hassam <zameer.hassam@transficc.com>
2021-08-04 08:53:17 +0100

    add in merlin file and improve test suite

--- ch3/sum_test.ml
+++ ch3/sum_test.ml
@@ -2,12 +2,12 @@ open OUnit2
 open Sum
 
 let make_sum_test name expected_output input =
-  name >:: (fun _ -> assert_equal expected_output (sum input) ~printer:string_of_int);
+  name >:: (fun _ -> assert_equal expected_output (sum input) ~printer:string_of_int)
 
 let tests = "test suite for sum" >::: [
-  make_sum_test "empty" >:: (fun _ -> assert_equal 0 (sum []) ~printer:string_of_int);
-  "one" >:: (fun _ -> assert_equal 1 (sum [1]) ~printer:string_of_int);
-  "onetwo" >:: (fun _ -> assert_equal 3 (sum [1; 2]) ~printer:string_of_int);
+  make_sum_test "empty" 0 [];
+  make_sum_test "one" 1 [1];
+  make_sum_test "onetwo" 3 [1; 2];
 ]
 
 let _ = run_test_tt_main tests
