611113e849d8863001bee577138f7640c1c13b5e
zameer.hassam <zam.hassam@googlemail.com>
2021-08-25 09:14:57 +0100

    more folding

--- ch4/exercises.ml
+++ ch4/exercises.ml
@@ -8,3 +8,15 @@ let rec repeat (f: 'a -> 'a) (n: int) (x: 'a) : 'a =
   match n with
   | 0 -> x
   | _ -> repeat f (n - 1) (f x)
+
+let product_left (x: float list) : float =
+  if x = [] then
+    1.0
+  else
+    List.fold_left ( *. ) 1.0 x
+
+let product_right (x: float list) : float =
+  if x = [] then
+    1.0
+  else
+    List.fold_right ( *. ) x 1.0
--- ch4/exercises_test.ml
+++ ch4/exercises_test.ml
@@ -4,7 +4,11 @@ open Exercises
 
 let tests = "test suite for exercises" >::: [
   "repeat_0" >:: (fun _ -> assert_equal 4 (repeat double 0 4) ~printer:string_of_int);
-  "repeat_2" >:: (fun _ -> assert_equal 16 (repeat double 2 4) ~printer:string_of_int)
+  "repeat_2" >:: (fun _ -> assert_equal 16 (repeat double 2 4) ~printer:string_of_int);
+  "product_left_empty" >:: (fun _ -> assert_equal 1.0 (product_left []) ~printer:string_of_float);
+  "product_left_1" >:: (fun _ -> assert_equal 5.0 (product_left [5.0]) ~printer:string_of_float);
+  "product_right_empty" >:: (fun _ -> assert_equal 1.0 (product_right []) ~printer:string_of_float);
+  "product_right_1" >:: (fun _ -> assert_equal 5.0 (product_right [5.0]) ~printer:string_of_float);
 ]
 
 let _ = run_test_tt_main tests
